64c72de5b00f01531539e8dd19ffc7dbf3199778
Omar Qureshi <omar@omarqureshi.net>
2017-10-03 10:54:44 +0100

    Some sort of half baked idea at implementing a cache system for token generation

--- lib/gatekeeper/client/oauth_client.rb
+++ lib/gatekeeper/client/oauth_client.rb
@@ -2,10 +2,49 @@ module Gatekeeper
   module Client
     class OAuthClient < Base
       class << self
+        def cache_backend=(cache)
+          @cache = cache
+        end
+
         def find(client_id: ENV['GATEKEEPER_CLIENT_ID'],
                  client_secret: ENV['GATEKEEPER_CLIENT_SECRET'],
                  username: nil,
                  password: nil)
+          access_token = nil
+          cached_client = find_from_cache(client_id)
+          if cached_client
+            cached_client
+          else
+            payload = generate_payload(client_id, client_secret, username, password)
+            response = connection.post do |request|
+              request.url '/tokens'
+              request.headers['Content-Type'] = 'application/json'
+              request.body = payload
+            end
+            body = JSON.parse(response.body)
+            access_token = body['access_token']
+            set_cache(key: client_id, value: access_token, expiry: body['expiry'])
+          end
+          # test the cache retrieval works
+          access_token = get_cache(key: client_id, fallback: access_token)
+          new(client_id, access_token)
+        end
+
+        # These are decent utility methods, move to Base?
+        def get_cache(key: nil, fallback: nil)
+          value = @cache.get(key) if @cache
+          value || fallback
+        end
+
+        def set_cache(key: nil, value: nil, expiry: nil)
+          if expiry
+            @cache.setex(key, expiry.to_i, value)
+          else
+            @cache.set(key, value)
+          end
+        end
+
+        def generate_payload(client_id, client_secret, username, password)
           payload = {client_id: client_id}
           if client_secret
             payload[:client_secret] = client_secret
@@ -15,21 +54,14 @@ module Gatekeeper
             payload[:username] = username
             payload[:password] = password
           end
-          response = connection.post do |request|
-            request.url '/tokens'
-            request.headers['Content-Type'] = 'application/json'
-            request.body = payload
-          end
-          body = JSON.parse(response.body)
-          new(client_id, client_secret, body['access_token'])
+          payload
         end
       end
 
       attr_reader :access_token
 
-      def initialize(client_id, client_secret, access_token)
+      def initialize(client_id, access_token)
         @client_id = client_id
-        @client_secret = client_secret
         @access_token = access_token
       end
     end
